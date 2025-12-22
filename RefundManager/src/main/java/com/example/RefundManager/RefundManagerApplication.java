870c849f28cb5aa76fb82155881c29b3c55bd361
Yuri Martins <yurimn14@outlook.com>
2025-12-22 15:00:41 -0300

    chore: add PostgreSQL docker-compose and datasource configuration

---
+++ RefundManager/src/main/java/com/example/RefundManager/RefundManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.RefundManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefundManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefundManagerApplication.class, args);
+	}
+
+}
---
+++ RefundManager/src/test/java/com/example/RefundManager/RefundManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RefundManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefundManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
