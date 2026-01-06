17bdace48411002cdecea66e5746820039bf217e
Yuri Martins <yurimn14@outlook.com>
2026-01-06 13:00:32 +0000

    Add DepartmentControllerIntegrationTest

--- ExpenseManager/src/test/java/com/ubs/ExpenseManager/controller/DepartmentControllerIntegrationTest.java
+++ ExpenseManager/src/test/java/com/ubs/ExpenseManager/controller/DepartmentControllerIntegrationTest.java
@@ -1,13 +1,24 @@
 package com.ubs.ExpenseManager.controller;
 
 import net.bytebuddy.utility.dispatcher.JavaDispatcher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
 public class DepartmentControllerIntegrationTest {
     @Container
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
             .withDatabaseName("testdb")
             .withUsername("testuser")
             .withPassword("testpassword");
+
+    @Autowired
+    private MockMvc mockMvc;
 }
